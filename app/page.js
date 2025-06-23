df1a3d8dd3b6fef820ef4ff9ad917bc07c522c23
Jose Andres Peña <joseandrespena@MacBook-Air-de-Jose.local>
2025-06-22 20:16:01 -0400

    Agrega página principal de bienvenida

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{textAlign: "center", marginTop: "5rem"}}>
+      <h1>Bienvenido a Sobrecupos MVP 👁️</h1>
+      <a href="/admin">Ir al panel de administración</a>
+    </main>
+  );
+}
