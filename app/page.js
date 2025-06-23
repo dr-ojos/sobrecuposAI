fd2c59b335fcdac76e1b99a69fc3429e3b8de612
Jose Andres Peña <joseandrespena@MacBook-Air-de-Jose.local>
2025-06-22 20:23:24 -0400

    Describe tu cambio aquí, por ejemplo: eliminar pages/index.js para evitar conflicto

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{textAlign: "center", marginTop: "5rem"}}>
-      <h1>Bienvenido a Sobrecupos MVP 👁️</h1>
+      <h1>Bienvenido a Sobrecupos AI Chatbot MVP 👁️</h1>
       <a href="/admin">Ir al panel de administración</a>
     </main>
   );
